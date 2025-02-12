27fbb90f24faa8020cf61a655e5c361f49d2d735
EFG <efg@efg.local>
2025-02-12 15:34:34 -0300

    fix: correcao de erros, estava faltando o arquivo bundle.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,28 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js', // Ponto de entrada do seu projeto
+  entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
+  },
+  devServer: {
+    static: path.join(__dirname, 'public'),
+    port: 3000,
+    hot: true,
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
+        use: 'babel-loader',
       },
       {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       },
     ],
-  },
-  devServer: {
-    static: path.join(__dirname, 'public'),
-    port: 3000,
-    hot: true,
   },
   resolve: {
     extensions: ['.js', '.jsx'],
