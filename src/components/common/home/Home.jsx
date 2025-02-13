c80d88a7e413a5540701626fe087fece7b664d84
JohnnTech <jonntech01@gmail.com>
2025-02-13 19:31:01 -0300

    fix: correcao de bugs

--- src/components/common/home/Home.jsx
+++ src/components/common/home/Home.jsx
@@ -1,40 +1,45 @@
-// import './Home.css';
-// import img from '../../../assets/props/img2.jpg';
-import {BsMouse} from 'react-icons/bs';
+import './Home.css';
+import img from '../../../assets/props/img2.jpg';
+// import Buttons from '../button/button';
+import { BsMouse } from 'react-icons/bs';
 
-function Home(){
-    return (
-        <div id='home' className="container home-container">
-            <div className="logo">
-                <div className="main-img">
-                    <span className="circle"></span>
-                    <span className="circle"></span>
-                    <span className="circle"></span>
-                    <span className="circle"></span>
-                    <span className="circle"></span>
-                    <span className="circle"></span>
-                    <span className="circle"></span>
-                    <span className="circle"></span>                    
-                </div>
-                {/* <img src='{img}' alt='' /> */}
-            </div>
-            <a href="#footer" className='scroll-down'>
-                <hr />
-                <h5>scroll down</h5>
-                <BsMouse className='scroll' />
-                <hr />
-            </a>
-            <h2><span>Sobre Min</span></h2>
-            <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae aliquid nostrum doloribus veniam minus ex nemo, eos libero commodi, earum assumenda nulla! Quasi placeat amet dolorem tempore architecto blanditiis mollitia.
-            </p>
+function Home() {
+  return (
+    <div id='home' className='container home-container'>
+      <div className='logo'>
+        <div className='hover-show'>
+          <span className='circle'></span>
+          <span className='circle'></span>
+          <span className='circle'></span>
+          <span className='circle'></span>
+          <span className='circle'></span>
+          <span className='circle'></span>
+          <span className='circle'></span>
+          <span className='circle'></span>
         </div>
-    );
-}
 
-const toggle = document.querySelector('.main-img');
-toggle.addEventListener('click', () => {
-    toggle.classList.toggle('active')
-})
+        <img src={img} alt='' />
+      </div>
+
+      <a href='#footer' className='scroll-down'>
+        <hr />
+        <h5>scroll down</h5>
+        <BsMouse className='scroll' />
+        <hr />
+      </a>
+
+      <h2>
+        <span>About Me</span> <br />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis id
+          quos deserunt, maxime sint sunt tempore quidem ad praesentium itaque,
+          aspernatur, magnam culpa iste adipisci?
+        </p>
+      </h2>
+
+      {/* <Buttons /> */}
+    </div>
+  );
+}
 
 export default Home;
