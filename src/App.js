df81954914e6ec30cb4fcbbb1aa405a0937abdf2
JohnnTech <jonntech01@gmail.com>
2025-02-12 22:54:31 -0300

    adicionei o component Navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-
-import Header from './components/common/Header';
-import './styles/global.css';
+// import './styles/global.css';
+import Header from './components/common/header/Header';
+import Navbar from './components/common/nav/Nav';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Navbar />
     </div>
   );
 }
