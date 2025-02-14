13174a2f90d75912d6e52ec34f24406317660a5a
JohnnTech <johnntech01@gmail.com>
2025-02-14 17:16:17 -0300

    troca de webpack para vite

---
+++ src/index.jsx
@@ -0,0 +1,31 @@
+import { createRoot } from 'react-dom/client';
+import './global.css';
+import WebContent from './App';
+
+const root = createRoot(document.getElementById('root'));
+root.render(<WebContent />);
+
+
+const toggle = document.querySelector('.hover-show');
+
+if(toggle){
+  toggle.addEventListener('click', () => {
+    toggle.classList.toggle('active');
+  });
+}
+let Icons = document.querySelectorAll('.navigation .icon');
+
+if(Icons){
+  Icons.forEach((icon) => {
+    icon.addEventListener('click', () => {
+      changeactive();
+      icon.classList.add('active-nav');
+    });
+  });
+
+  function changeactive() {
+    Icons.forEach((icon) => {
+      icon.classList.remove('active-nav');
+    });
+  }
+}
